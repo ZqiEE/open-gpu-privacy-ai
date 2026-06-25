1fefc25fd444d536d0fc5dcc09b47bcb2f9b8252
ZqiEE <18942619472@163.com>
2026-06-25 13:58:49 +0800

    include autonomous api

--- api/main_learning.py
+++ api/main_learning.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from api.autonomous_api import router as autonomous_router
 from api.learning_api import router as learning_router
 from api.learning_foundation_api import router as learning_foundation_router
 from api.learning_gate_api import router as learning_gate_router
@@ -12,3 +13,4 @@ app.include_router(learning_foundation_router)
 app.include_router(learning_gate_router)
 app.include_router(model_monitor_router)
 app.include_router(rollback_router)
+app.include_router(autonomous_router)
