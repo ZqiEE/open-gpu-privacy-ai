b71ae67cd66439a6191af1edec7b51fa710ad676
ZqiEE <18942619472@163.com>
2026-06-25 13:56:17 +0800

    include rollback api

--- api/main_learning.py
+++ api/main_learning.py
@@ -5,8 +5,10 @@ from api.learning_foundation_api import router as learning_foundation_router
 from api.learning_gate_api import router as learning_gate_router
 from api.main_packet import app
 from api.model_monitor_api import router as model_monitor_router
+from api.rollback_api import router as rollback_router
 
 app.include_router(learning_router)
 app.include_router(learning_foundation_router)
 app.include_router(learning_gate_router)
 app.include_router(model_monitor_router)
+app.include_router(rollback_router)
