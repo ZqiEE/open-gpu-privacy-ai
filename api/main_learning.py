689cce6d362795bd62eac8fb732520aabe2cf07c
ZqiEE <18942619472@163.com>
2026-06-25 17:39:23 +0800

    Include route book API

--- api/main_learning.py
+++ api/main_learning.py
@@ -12,6 +12,7 @@ from api.model_monitor_api import router as model_monitor_router
 from api.rflow2_api import router as rflow2_router
 from api.rflow_api import router as rflow_router
 from api.rollback_api import router as rollback_router
+from api.route_book_api import router as route_book_router
 
 app.include_router(learning_router)
 app.include_router(learning_foundation_router)
@@ -24,3 +25,4 @@ app.include_router(apply_router)
 app.include_router(rflow_router)
 app.include_router(rflow2_router)
 app.include_router(gg_router)
+app.include_router(route_book_router)
