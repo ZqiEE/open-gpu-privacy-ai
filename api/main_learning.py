c1969cd84f7c66cf6e0c1ee2fc8473e2d44ef45b
ZqiEE <18942619472@163.com>
2026-06-25 16:23:51 +0800

    Include rflow API

--- api/main_learning.py
+++ api/main_learning.py
@@ -8,6 +8,7 @@ from api.learning_foundation_api import router as learning_foundation_router
 from api.learning_gate_api import router as learning_gate_router
 from api.main_packet import app
 from api.model_monitor_api import router as model_monitor_router
+from api.rflow_api import router as rflow_router
 from api.rollback_api import router as rollback_router
 
 app.include_router(learning_router)
@@ -18,3 +19,4 @@ app.include_router(rollback_router)
 app.include_router(autonomous_router)
 app.include_router(artifact_binding_router)
 app.include_router(apply_router)
+app.include_router(rflow_router)
