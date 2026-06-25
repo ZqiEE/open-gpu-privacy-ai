2071bd1cdabd50f3e170779008b646c92c405314
ZqiEE <18942619472@163.com>
2026-06-25 13:40:38 +0800

    include learning foundation api

--- api/main_learning.py
+++ api/main_learning.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 
 from api.learning_api import router as learning_router
+from api.learning_foundation_api import router as learning_foundation_router
 from api.main_packet import app
 
 app.include_router(learning_router)
+app.include_router(learning_foundation_router)
