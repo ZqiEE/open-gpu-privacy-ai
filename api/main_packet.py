cc2ec674e7776974f7f5b863192b9e5af667193e
ZqiEE <18942619472@163.com>
2026-06-25 16:31:09 +0800

    Include outbox API

--- api/main_packet.py
+++ api/main_packet.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 
 from api.main_owned import app
+from api.outbox_api import router as outbox_router
 from api.parcel_routes import router
 
 app.include_router(router)
+app.include_router(outbox_router)
