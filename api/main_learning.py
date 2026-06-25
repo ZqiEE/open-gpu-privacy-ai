4ff170b49afc573a7d7a44e512d3b1b692fa0d0c
ZqiEE <18942619472@163.com>
2026-06-25 15:17:05 +0800

    Include ref API

--- api/main_learning.py
+++ api/main_learning.py
@@ -7,6 +7,7 @@ from api.learning_foundation_api import router as learning_foundation_router
 from api.learning_gate_api import router as learning_gate_router
 from api.main_packet import app
 from api.model_monitor_api import router as model_monitor_router
+from api.ref_api import router as ref_router
 from api.rollback_api import router as rollback_router
 
 app.include_router(learning_router)
@@ -16,3 +17,4 @@ app.include_router(model_monitor_router)
 app.include_router(rollback_router)
 app.include_router(autonomous_router)
 app.include_router(artifact_binding_router)
+app.include_router(ref_router)
