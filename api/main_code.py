1fab6fc78c27fe21fa92026d8ccf2fb583306dec
ZqiEE <18942619472@163.com>
2026-06-27 22:38:31 +0800

    include forward routes

--- api/main_code.py
+++ api/main_code.py
@@ -6,6 +6,7 @@ from api.catalog_extra_routes import router as extra_router
 from api.code_chat_api import router as code_chat_router
 from api.code_result_api import router as code_result_router
 from api.runtime_extra_routes import router as rt_router
+from api.runtime_forward_routes import router as fw_router
 from api.main import app
 
 app.include_router(code_chat_router)
@@ -14,3 +15,4 @@ app.include_router(catalog_router)
 app.include_router(extra_router)
 app.include_router(rt_router)
 app.include_router(benchmark_router)
+app.include_router(fw_router)
