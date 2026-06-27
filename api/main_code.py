c8327ce4160350366868654fa5d855dcc15deded
ZqiEE <18942619472@163.com>
2026-06-27 22:30:14 +0800

    wire local routes

--- api/main_code.py
+++ api/main_code.py
@@ -4,9 +4,11 @@ from api.catalog_routes import router as catalog_router
 from api.catalog_extra_routes import router as extra_router
 from api.code_chat_api import router as code_chat_router
 from api.code_result_api import router as code_result_router
+from api.runtime_extra_routes import router as rt_router
 from api.main import app
 
 app.include_router(code_chat_router)
 app.include_router(code_result_router)
 app.include_router(catalog_router)
 app.include_router(extra_router)
+app.include_router(rt_router)
