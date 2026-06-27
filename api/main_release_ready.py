b8b65fe3eacb9b1a4251078c80b3436d49793325
ZqiEE <18942619472@163.com>
2026-06-28 02:24:27 +0800

    mount default chat readiness route

--- api/main_release_ready.py
+++ api/main_release_ready.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 
+from api.default_chat_probe_routes import router as default_chat_probe_router
 from api.final_gate_routes import router as final_gate_router
 from api.main_incident_ready import app
 
 app.include_router(final_gate_router)
+app.include_router(default_chat_probe_router)
