7bdc1b969372d6e02aeb53eff16a62c0a0630c8e
ZqiEE <18942619472@163.com>
2026-06-27 22:19:40 +0800

    include catalog routes

--- api/main_code.py
+++ api/main_code.py
@@ -1,8 +1,10 @@
 from __future__ import annotations
 
+from api.catalog_routes import router as catalog_router
 from api.code_chat_api import router as code_chat_router
 from api.code_result_api import router as code_result_router
 from api.main import app
 
 app.include_router(code_chat_router)
 app.include_router(code_result_router)
+app.include_router(catalog_router)
